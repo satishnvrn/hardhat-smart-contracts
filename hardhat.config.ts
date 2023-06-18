e394f127e78837fa24906d30baa5acf1856babc3
satishnvrn <nvrn.satish@gmail.com>
2023-06-19 01:02:59 +0530

    Added sepolia network configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'dotenv/config';
 
-const SEPOLIA_RPC_URL: string = process.env.SEPOLIA_RPC_URL!;
-const PRIVATE_KEY: string = process.env.PRIVATE_KEY!;
+const SEPOLIA_RPC_URL: string = process.env.SEPOLIA_RPC_URL || '';
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY || '';
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
