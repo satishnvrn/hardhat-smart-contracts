a8d44904f70421e310ed031771e8ca79784ce9c3
satishnvrn <nvrn.satish@gmail.com>
2023-06-24 16:00:13 +0530

    Added verification for the contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'dotenv/config';
+import "@nomicfoundation/hardhat-verify";
 
 const SEPOLIA_RPC_URL: string = process.env.SEPOLIA_RPC_URL || '';
 const PRIVATE_KEY: string = process.env.PRIVATE_KEY || '';
+const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY || '';
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
@@ -11,6 +14,11 @@ const config: HardhatUserConfig = {
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 11155111
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
     }
   }
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,26 @@
-import { ethers } from 'hardhat';
+import { ethers, run, network } from 'hardhat';
+
+async function verify(contractAddress: string, args: any) {
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error: any) {
+    if(error.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified");
+    } else {
+      console.log(error);
+    }
+  }
+}
+
+async function interact(contract: any) {
+  const transactionResponse = await contract.store(10);
+  await transactionResponse.wait(1);
+  const updatedValue = await contract.retrieve();
+  console.log('updatedValue', updatedValue);
+}
 
 async function main() {
   const simpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
@@ -7,7 +29,16 @@ async function main() {
   const simpleStorage = await simpleStorageFactory.deploy();
   await simpleStorage.waitForDeployment();
   const deployedAddress = await simpleStorage.getAddress();
+  const transactionResponse = await simpleStorage.deploymentTransaction();
   console.log('Deployed simple storage to', deployedAddress);
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY && transactionResponse) {
+    console.log('waiting for txes');
+    await transactionResponse.wait(20);
+    console.log('20 transactions approved!')
+    await verify(deployedAddress, []);
+  }
+
+  await interact(simpleStorage);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
